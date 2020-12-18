be63fe41de1d26c052bc68a6217cde964e01bf9f
Ben G <57531588+bengoe@users.noreply.github.com>
2020-12-18 16:05:22 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ version=$(wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/versio
 #VersionCheck
 if [ "$version" = "$currentversion" ]; then
 echo "Newest version installed"
+sleep 1
+sh /etc/drmg/scripts
+echo "Was soll installiert werden?"
+read -p "" var1
+sh /etc/drmg/$var1.sh
 else
 echo "Old version installed. Auto-Updating now!"
 rm -r /etc/drmg
