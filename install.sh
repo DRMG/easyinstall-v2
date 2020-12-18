54b6c58a4433a23a80fe20ef0e8b09c6ef8c5ac7
Ben G <57531588+bengoe@users.noreply.github.com>
2020-12-18 15:53:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ wget https://drmg.github.io/EasyInstall/run.sh -O run.sh -q
 wget https://drmg.github.io/EasyInstall/scripts.sh -O scripts.sh -q
 wget https://drmg.github.io/update.sh -O update.sh -q
 wget https://drmg.github.io/start.sh -O programm -q
-wget https://drmg.github.io/EasyInstall/uninstall.sh -O uninstall.sh -q
+wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/uninstaller.sh -O uninstall.sh -q
 wget https://drmg.github.io/whoisconnected.sh -O whoisconnected.sh -q
 wget https://drmg.github.io/apachemysql.sh -O apachemysql.sh -q
 wget https://drmg.github.io/nextcloud.sh -O nextcloud.sh -q
 wget https://drmg.github.io/mc.sh -O mc-server.sh -q
 #Get the starter
 cd /bin
-wget 
+wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/runbash.sh -O drmg -q
