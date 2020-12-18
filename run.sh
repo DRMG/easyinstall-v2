8eaf771fbf4467c0c226cdc6ef4656620e13cdfd
Ben G <57531588+bengoe@users.noreply.github.com>
2020-12-18 15:58:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #Vars
-$currentversion=0.1
-$version=$(wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/version.txt -q -O -)
+currentversion=0.1
+version=$(wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/version.txt -q -O -)
 #VersionCheck
 if [ "$version" = "$currentversion" ] then
 echo "Newest version installed"
