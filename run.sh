c93d900590ffaca978582d77c41cbdc4f6f97111
Ben G <57531588+bengoe@users.noreply.github.com>
2020-12-18 15:59:11 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 currentversion=0.1
 version=$(wget https://raw.githubusercontent.com/DRMG/easyinstall-v2/main/version.txt -q -O -)
 #VersionCheck
-if [ "$version" = "$currentversion" ] then
+if [ "$version" = "$currentversion" ]; then
 echo "Newest version installed"
 else
 echo "Old version installed. Auto-Updating now!"
